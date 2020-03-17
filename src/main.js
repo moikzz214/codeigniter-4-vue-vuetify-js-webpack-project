a22ab56ab8a9eb472d4c15ca2444e95adf072895
moikzz214 <moikzz214@gmail.com>
2020-03-17 08:34:06 +0400

    Updated file

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import '@babel/polyfill'
 import Vue from 'vue'
 import vuetify from './plugins/vuetify' // path to vuetify export
 import './sass/custom.scss' // path to custom sass
-import axios from 'axios'
-
-//window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+ 
+window.axios = require('axios'); 
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 Vue.component('registration-form', require('./components/RegistrationForm.vue').default);
 new Vue({
   vuetify, 
